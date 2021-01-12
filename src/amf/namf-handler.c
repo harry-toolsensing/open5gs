2cf12f01dcf8796d6817d765db52d30b35656d5f
Sukchan Lee <acetcom@gmail.com>
2021-01-12 09:45:20 -0500

    update it

--- src/amf/namf-handler.c
+++ src/amf/namf-handler.c
@@ -28,13 +28,14 @@ int amf_namf_comm_handle_n1_n2_message_transfer(
         ogs_sbi_stream_t *stream, ogs_sbi_message_t *recvmsg)
 {
     int status;
+    OpenAPI_n1_n2_message_transfer_cause_e cause;
 
     amf_ue_t *amf_ue = NULL;
     ran_ue_t *ran_ue = NULL;
     amf_sess_t *sess = NULL;
 
-    ogs_pkbuf_t *n1smbuf = NULL;
-    ogs_pkbuf_t *n2smbuf = NULL;
+    ogs_pkbuf_t *n1buf = NULL;
+    ogs_pkbuf_t *n2buf = NULL;
 
     ogs_pkbuf_t *gmmbuf = NULL;
     ogs_pkbuf_t *ngapbuf = NULL;
@@ -60,47 +61,6 @@ int amf_namf_comm_handle_n1_n2_message_transfer(
     N1N2MessageTransferReqData = recvmsg->N1N2MessageTransferReqData;
     if (!N1N2MessageTransferReqData) {
         ogs_error("No N1N2MessageTransferReqData");
-        return OGS_ERROR;
-    }
-
-    n1MessageContainer = N1N2MessageTransferReqData->n1_message_container;
-    if (!n1MessageContainer) {
-        ogs_error("No n1MessageContainer");
-        return OGS_ERROR;
-    }
-    n1MessageContent = n1MessageContainer->n1_message_content;
-    if (!n1MessageContent || !n1MessageContent->content_id) {
-        ogs_error("No n1MessageContent");
-        return OGS_ERROR;
-    }
-
-    n2InfoContainer = N1N2MessageTransferReqData->n2_info_container;
-    if (!n2InfoContainer) {
-        ogs_error("No n2InfoContainer");
-        return OGS_ERROR;
-    }
-    smInfo = n2InfoContainer->sm_info;
-    if (!smInfo) {
-        ogs_error("No smInfo");
-        return OGS_ERROR;
-    }
-    n2InfoContent = smInfo->n2_info_content;
-    if (!n2InfoContent) {
-        ogs_error("No n2InfoContent");
-        return OGS_ERROR;
-    }
-    switch (n2InfoContent->ngap_ie_type) {
-    case OpenAPI_ngap_ie_type_PDU_RES_SETUP_REQ:
-    case OpenAPI_ngap_ie_type_PDU_RES_MOD_REQ:
-        break;
-    default:
-        ogs_error("Not implemented ngap_ie_type[%d]",
-                n2InfoContent->ngap_ie_type);
-        return OGS_ERROR;
-    }
-    ngapData = n2InfoContent->ngap_data;
-    if (!ngapData || !ngapData->content_id) {
-        ogs_error("No ngapData");
         return OGS_ERROR;
     }
 
@@ -132,36 +92,80 @@ int amf_namf_comm_handle_n1_n2_message_transfer(
         return OGS_ERROR;
     }
 
-    n1smbuf = ogs_sbi_find_part_by_content_id(
-            recvmsg, n1MessageContent->content_id);
-    if (!n1smbuf) {
-        ogs_error("[%s] No N1 SM Content", amf_ue->supi);
-        return OGS_ERROR;
-    }
-    /*
-     * NOTE : The pkbuf created in the SBI message will be removed
-     *        from ogs_sbi_message_free(), so it must be copied.
-     */
-    n1smbuf = ogs_pkbuf_copy(n1smbuf);
-    ogs_assert(n1smbuf);
+    n1MessageContainer = N1N2MessageTransferReqData->n1_message_container;
+    if (n1MessageContainer) {
+        n1MessageContent = n1MessageContainer->n1_message_content;
+        if (!n1MessageContent || !n1MessageContent->content_id) {
+            ogs_error("No n1MessageContent");
+            return OGS_ERROR;
+        }
 
-    n2smbuf = ogs_sbi_find_part_by_content_id(recvmsg, ngapData->content_id);
-    if (!n2smbuf) {
-        ogs_error("[%s] No N2 SM Content", amf_ue->supi);
+        n1buf = ogs_sbi_find_part_by_content_id(
+                recvmsg, n1MessageContent->content_id);
+        if (!n1buf) {
+            ogs_error("[%s] No N1 SM Content", amf_ue->supi);
+            return OGS_ERROR;
+        }
+
+        /*
+         * NOTE : The pkbuf created in the SBI message will be removed
+         *        from ogs_sbi_message_free(), so it must be copied.
+         */
+        n1buf = ogs_pkbuf_copy(n1buf);
+        ogs_assert(n1buf);
+    }
+
+    n2InfoContainer = N1N2MessageTransferReqData->n2_info_container;
+    if (n2InfoContainer) {
+        smInfo = n2InfoContainer->sm_info;
+        if (!smInfo) {
+            ogs_error("No smInfo");
+            return OGS_ERROR;
+        }
+        n2InfoContent = smInfo->n2_info_content;
+        if (!n2InfoContent) {
+            ogs_error("No n2InfoContent");
+            return OGS_ERROR;
+        }
+
+        ngapData = n2InfoContent->ngap_data;
+        if (!ngapData || !ngapData->content_id) {
+            ogs_error("No ngapData");
+            return OGS_ERROR;
+        }
+        n2buf = ogs_sbi_find_part_by_content_id(
+                recvmsg, ngapData->content_id);
+        if (!n2buf) {
+            ogs_error("[%s] No N2 SM Content", amf_ue->supi);
+            return OGS_ERROR;
+        }
+
+        /*
+         * NOTE : The pkbuf created in the SBI message will be removed
+         *        from ogs_sbi_message_free(), so it must be copied.
+         */
+        n2buf = ogs_pkbuf_copy(n2buf);
+        ogs_assert(n2buf);
+    }
+
+    switch (n2InfoContent->ngap_ie_type) {
+    case OpenAPI_ngap_ie_type_PDU_RES_SETUP_REQ:
+    case OpenAPI_ngap_ie_type_PDU_RES_MOD_REQ:
+        /* N1 SM Message */
+        if (n1buf) {
+            gmmbuf = gmm_build_dl_nas_transport(sess,
+                    OGS_NAS_PAYLOAD_CONTAINER_N1_SM_INFORMATION, n1buf, 0, 0);
+            ogs_assert(gmmbuf);
+        }
+        break;
+    default:
+        ogs_error("Not implemented ngap_ie_type[%d]",
+                n2InfoContent->ngap_ie_type);
         return OGS_ERROR;
     }
-    /*
-     * NOTE : The pkbuf created in the SBI message will be removed
-     *        from ogs_sbi_message_free(), so it must be copied.
-     */
-    n2smbuf = ogs_pkbuf_copy(n2smbuf);
-    ogs_assert(n2smbuf);
 
     status = OGS_SBI_HTTP_STATUS_OK;
-
-    gmmbuf = gmm_build_dl_nas_transport(sess,
-            OGS_NAS_PAYLOAD_CONTAINER_N1_SM_INFORMATION, n1smbuf, 0, 0);
-    ogs_assert(gmmbuf);
+    cause = OpenAPI_n1_n2_message_transfer_cause_N1_N2_TRANSFER_INITIATED;
 
     switch (n2InfoContent->ngap_ie_type) {
     case OpenAPI_ngap_ie_type_PDU_RES_SETUP_REQ:
@@ -172,11 +176,11 @@ int amf_namf_comm_handle_n1_n2_message_transfer(
 
         if (ran_ue->initial_context_setup_request_sent == true) {
             ngapbuf = ngap_sess_build_pdu_session_resource_setup_request(
-                    sess, gmmbuf, n2smbuf);
+                    sess, gmmbuf, n2buf);
             ogs_assert(ngapbuf);
         } else {
             ngapbuf = ngap_sess_build_initial_context_setup_request(
-                    sess, gmmbuf, n2smbuf);
+                    sess, gmmbuf, n2buf);
             ogs_assert(ngapbuf);
 
             ran_ue->initial_context_setup_request_sent = true;
@@ -202,7 +206,7 @@ int amf_namf_comm_handle_n1_n2_message_transfer(
 
     case OpenAPI_ngap_ie_type_PDU_RES_MOD_REQ:
         ngapbuf = ngap_build_pdu_session_resource_modify_request(
-                sess, gmmbuf, n2smbuf);
+                sess, gmmbuf, n2buf);
         ogs_assert(ngapbuf);
 
         if (nas_5gs_send_to_gnb(amf_ue, ngapbuf) != OGS_OK) {
@@ -217,13 +221,24 @@ int amf_namf_comm_handle_n1_n2_message_transfer(
         ogs_assert_if_reached();
     }
 
+
+#if 0
+    if (CM_IDLE(amf_ue)) {
+        ogs_fatal("Not implemented");
+        ogs_assert_if_reached();
+    } else if (CM_CONNECTED(amf_ue)) {
+    } else {
+        ogs_fatal("[%s] Invalid RAN-UE : [%p]", amf_ue->supi, amf_ue->ran_ue);
+        ogs_assert_if_reached();
+    }
+#endif
+
     memset(&sendmsg, 0, sizeof(sendmsg));
 
     if (status == OGS_SBI_HTTP_STATUS_OK) {
         memset(&N1N2MessageTransferRspData, 0,
                 sizeof(N1N2MessageTransferRspData));
-        N1N2MessageTransferRspData.cause =
-            OpenAPI_n1_n2_message_transfer_cause_N1_N2_TRANSFER_INITIATED;
+        N1N2MessageTransferRspData.cause = cause;
 
         sendmsg.N1N2MessageTransferRspData = &N1N2MessageTransferRspData;
     }
