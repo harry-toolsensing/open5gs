3c1af267a58ea997beb7b8313a68155ccf38741d
Sukchan Lee <acetcom@gmail.com>
2021-01-16 11:43:22 -0500

    update it

--- src/smf/namf-handler.c
+++ src/smf/namf-handler.c
@@ -53,11 +53,13 @@ bool smf_namf_comm_handler_n1_n2_message_transfer(
 
     case SMF_NETWORK_TRIGGERED_SERVICE_REQUEST:
         if (recvmsg->res_status == OGS_SBI_HTTP_STATUS_OK) {
-            ogs_fatal("OK");
             /* Nothing */
         } else if (recvmsg->res_status == OGS_SBI_HTTP_STATUS_ACCEPTED) {
-            ogs_error("ACCEPT");
-            /* Nothing */
+            if (recvmsg->http.location)
+                smf_sess_set_paging_n1n2message_location(
+                        sess, recvmsg->http.location);
+            else
+                ogs_error("No HTTP Location");
         } else {
             ogs_error("[%s:%d] HTTP response error [%d]",
                 smf_ue->supi, sess->psi, recvmsg->res_status);
@@ -69,5 +71,51 @@ bool smf_namf_comm_handler_n1_n2_message_transfer(
         ogs_assert_if_reached();
     }
 
+    return true;
+}
+
+bool smf_namf_comm_handler_n1_n2_message_transfer_failure_notify(
+        ogs_sbi_stream_t *stream, ogs_sbi_message_t *recvmsg)
+{
+    OpenAPI_n1_n2_msg_txfr_failure_notification_t
+        *N1N2MsgTxfrFailureNotification = NULL;
+
+    smf_sess_t *sess = NULL;
+
+    ogs_assert(stream);
+    ogs_assert(recvmsg);
+
+    N1N2MsgTxfrFailureNotification = recvmsg->N1N2MsgTxfrFailureNotification;
+    if (!N1N2MsgTxfrFailureNotification) {
+        ogs_error("No N1N2MsgTxfrFailureNotification");
+        ogs_sbi_server_send_error(stream, OGS_SBI_HTTP_STATUS_BAD_REQUEST,
+            recvmsg, "No N1N2MsgTxfrFailureNotification", NULL);
+        return false;
+    }
+
+    if (!N1N2MsgTxfrFailureNotification->cause) {
+        ogs_error("No Cause");
+        ogs_sbi_server_send_error(stream, OGS_SBI_HTTP_STATUS_BAD_REQUEST,
+            recvmsg, "No Cause", NULL);
+        return false;
+    }
+
+    if (!N1N2MsgTxfrFailureNotification->n1n2_msg_data_uri) {
+        ogs_error("No n1n2MsgDataUri");
+        ogs_sbi_server_send_error(stream, OGS_SBI_HTTP_STATUS_BAD_REQUEST,
+            recvmsg, "No n1n2MsgDataUri", NULL);
+        return false;
+    }
+
+    sess = smf_sess_find_by_paging_n1n2message_location(
+        N1N2MsgTxfrFailureNotification->n1n2_msg_data_uri);
+    if (!sess) {
+        ogs_error("Not found");
+        ogs_sbi_server_send_error(stream, OGS_SBI_HTTP_STATUS_NOT_FOUND,
+            recvmsg, N1N2MsgTxfrFailureNotification->n1n2_msg_data_uri, NULL);
+        return false;
+    }
+
+    smf_sbi_send_http_status_no_content(stream);
     return true;
 }
